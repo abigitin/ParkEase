36e8667c2ab9ec6930d791167f9f1ffe0f6c5ab4
Arpita Podder <97333623+arpitapodder6@users.noreply.github.com>
2023-09-23 22:19:01 +0530

    updated Payment page and added Payment success.

---
+++ src/components/PAY/PaymentSuccess.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PaymentSuccessful() {
+
+
+  return (
+    <>
+        <div>
+            <h2>Payment Successful!</h2>
+            <p>Your payment has been successfully processed.</p>
+        </div>
+    </>
+  )
+}
+
+export default PaymentSuccessful;
