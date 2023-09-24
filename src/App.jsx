66e8db495e50cdd791476400d75ef7d934d627b8
Kaushik2105 <karmakark1267@gmail.com>
2023-09-24 21:37:36 +0530

    Email Verification

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import SignUp from "./components/AUTH/SignUp";
 import Home from "./components/Home/Home";
 import Payment from "./components/PAY/Payment";
 import Paymentscs from "./components/PAY/PaymentSuccess";
+import Info from "./components/Info";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<SignIn />} />
-					<Route path="/signup" element={<SignUp />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/payment" element={<Payment />} />
-					<Route path="/payment/scs" element={<Paymentscs />} />
-					<Route path="/scs" element={<Paymentscs />} />
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/payment/scs" element={<Paymentscs />} />
+          <Route path="/scs" element={<Paymentscs />} />
+          <Route path="/info" element={<Info />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
