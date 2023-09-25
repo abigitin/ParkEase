21c279b1c5d87f64daa4bb525fa5eb50add221f9
Almaz-111 <almazakhtat111@gmail.com>
2023-09-25 21:43:50 +0530

    Sign up done

--- src/components/AUTH/SignUp.jsx
+++ src/components/AUTH/SignUp.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import loginImg from "../../images/loginImg.png";
+import apple_icon from "../../images/apple_icon.png";
+import facebookIcon from "../../images/facebook_icon.png";
+import googleIcon from "../../images/google_icon.png";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyC7q9dDSeSXr9KfAbA9CL0zy6-7zBoop_w",
@@ -50,38 +54,58 @@ function SignUp() {
 						</p>
 						PARK <span className="text-white mx-2"> EASE</span>
 					</header>
-					<h1 className="text-yellow-300 font-mono text-3xl flex justify-center">
-						Sign-Up
-					</h1>
-					<div className="flex justify-center space-y-2 space-x-3">
-						<input
-							id="email"
-							type="email"
-							placeholder="Email"
-							value={email}
-							onChange={(e) => {
-								setEmail(e.target.value);
-							}}
-						/>
-						<input
-							id="password"
-							type="password"
-							placeholder="Password"
-							value={password}
-							onChange={(e) => {
-								setPassword(e.target.value);
-							}}
-						/>
-						<button
-							type="submit"
-							id="signup"
-							className="bg-yellow-400 rounded-r-md m-4"
-						>
-							Continue
-						</button>
-						<button className="bg-slate-300 rounded-r-md m-4">
-							Continue with Google
-						</button>
+
+					<div className="flex items-center justify-center">
+						<img src={loginImg} className="h-72 w-full" alt="Login image" />
+					</div>
+		
+					<p className="text-yellow-400 text-4xl text-left font-bold text-center pt-6	">
+						Sign Up
+					</p>
+					<div className="block justify-center items-center ">
+						<div className="flex justify-center items-center ml-10 mt-6 w-3/4">
+							<input 
+							type="text"
+							className="block border border-grey-light w-full p-3 rounded mb-4"
+							name="fullname"
+							placeholder="Full Name" />
+						</div>
+
+						<div className="flex justify-center items-center ml-10 mt-2 w-3/4">
+						<input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+						</div>
+                
+						<div className="flex justify-center items-center ml-10 mt-2 w-3/4">
+						<input 
+                        type="password"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+						</div>
+
+                       <p className="text-yellow-400 text-center text-2xl">Sign in with</p>
+                        <div className="flex  w-[10rem]  justify-center gap-5 ml-[7rem] mt-3">
+							<a href="" className=""><img  src={apple_icon} alt="appleIcon" /></a>
+							<a href="" className=""><img src={facebookIcon} alt="facebookIcon" /></a>
+							<a href="" className=""><img src={googleIcon} alt="googleIcon" /></a>
+						</div>
+						<div className="flex justify-center items-center">
+							<button
+								type="submit"
+								id="signup"
+								className="bg-yellow-400 rounded-md m-4 mb-0 w-4/5 py-2 text-2xl"
+							>
+								Sign Up
+							</button>
+						</div>
+						<p className="text-white mt-4 text-center tracking-widest text-lg">
+							Already have an account? <span className="text-yellow-400">Login</span>
+						</p>
+						
 					</div>
 				</div>
 			</form>
