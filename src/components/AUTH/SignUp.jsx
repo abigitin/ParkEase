7fd75ffc2f3fee60b0ed580035d24cb073bcb8ae
SK.ABIR AHMED <140634785+abigitin@users.noreply.github.com>
2023-09-26 01:29:41 +0530

    Update SignUp

--- src/components/AUTH/SignUp.jsx
+++ src/components/AUTH/SignUp.jsx
@@ -4,10 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import loginImg from "../../images/loginImg.png";
-import apple_icon from "../../images/apple_icon.png";
-import facebookIcon from "../../images/facebook_icon.png";
-import googleIcon from "../../images/google_icon.png";
-
+import { Link } from "react-router-dom";
 const firebaseConfig = {
 	apiKey: "AIzaSyC7q9dDSeSXr9KfAbA9CL0zy6-7zBoop_w",
 	authDomain: "parkease-login-e90c0.firebaseapp.com",
@@ -54,58 +51,52 @@ function SignUp() {
 						</p>
 						PARK <span className="text-white mx-2"> EASE</span>
 					</header>
-
 					<div className="flex items-center justify-center">
 						<img src={loginImg} className="h-72 w-full" alt="Login image" />
 					</div>
-		
-					<p className="text-yellow-400 text-4xl text-left font-bold text-center pt-6	">
+					<p className="text-yellow-400 text-4xl font-bold text-center pt-6">
 						Sign Up
 					</p>
 					<div className="block justify-center items-center ">
 						<div className="flex justify-center items-center ml-10 mt-6 w-3/4">
-							<input 
-							type="text"
-							className="block border border-grey-light w-full p-3 rounded mb-4"
-							name="fullname"
-							placeholder="Full Name" />
+							<input
+								id="email"
+								type="email"
+								placeholder="Email"
+								value={email}
+								className="block border border-grey-light w-full p-3 rounded mb-4"
+								onChange={(e) => {
+									setEmail(e.target.value);
+								}}
+							/>
 						</div>
-
 						<div className="flex justify-center items-center ml-10 mt-2 w-3/4">
-						<input 
-                        type="text"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="email"
-                        placeholder="Email" />
-						</div>
-                
-						<div className="flex justify-center items-center ml-10 mt-2 w-3/4">
-						<input 
-                        type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="password"
-                        placeholder="Password" />
-						</div>
-
-                       <p className="text-yellow-400 text-center text-2xl">Sign in with</p>
-                        <div className="flex  w-[10rem]  justify-center gap-5 ml-[7rem] mt-3">
-							<a href="" className=""><img  src={apple_icon} alt="appleIcon" /></a>
-							<a href="" className=""><img src={facebookIcon} alt="facebookIcon" /></a>
-							<a href="" className=""><img src={googleIcon} alt="googleIcon" /></a>
+							<input
+								id="password"
+								type="password"
+								placeholder="Password"
+								value={password}
+								className="block border border-grey-light w-full p-3 rounded mb-4"
+								onChange={(e) => {
+									setPassword(e.target.value);
+								}}
+							/>
 						</div>
 						<div className="flex justify-center items-center">
 							<button
 								type="submit"
 								id="signup"
-								className="bg-yellow-400 rounded-md m-4 mb-0 w-4/5 py-2 text-2xl"
+								className="bg-yellow-400 rounded-md mb-3 px-5 py-2 text-2xl"
 							>
 								Sign Up
 							</button>
 						</div>
-						<p className="text-white mt-4 text-center tracking-widest text-lg">
-							Already have an account? <span className="text-yellow-400">Login</span>
-						</p>
-						
+						<Link to="/">
+							<p className="text-white mt-2 text-center text-lg">
+								Already have an account?{" "}
+								<span className="text-yellow-400">Login</span>
+							</p>
+						</Link>
 					</div>
 				</div>
 			</form>
