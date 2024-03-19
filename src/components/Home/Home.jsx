d57186457162f1173e1ad6a0c364db04afdedcb5
Kaushik2105 <karmakark1267@gmail.com>
2024-03-19 13:34:32 +0530

    add: deploy workflow

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,128 +9,128 @@ import { useMediaQuery } from "@react-hook/media-query";
 import list from "../../../public/list.json";
 
 function Home() {
-	const [showPopup, togglePopup] = React.useState(false);
-	const [Latitude, setLatitude] = useState(null);
-	const [Longitude, setLongitude] = useState(null);
-	const [showMap, setShowMap] = useState(true);
-	const isSmallScreen = useMediaQuery("(max-width: 640px)");
-	const isMediumScreen = useMediaQuery(
-		"(min-width: 641px) and (max-width: 1024px)"
-	);
-	const [height, setHeight] = useState(535);
-	const [width, setWidth] = useState(1280);
+  const [showPopup, togglePopup] = React.useState(false);
+  const [Latitude, setLatitude] = useState(null);
+  const [Longitude, setLongitude] = useState(null);
+  const [showMap, setShowMap] = useState(true);
+  const isSmallScreen = useMediaQuery("(max-width: 640px)");
+  const isMediumScreen = useMediaQuery(
+    "(min-width: 641px) and (max-width: 1024px)"
+  );
+  const [height, setHeight] = useState(535);
+  const [width, setWidth] = useState(1280);
 
-	useEffect(() => {
-		if (isSmallScreen) {
-			setHeight(800);
-			setWidth(360);
-		} else if (isMediumScreen) {
-			setHeight(1800);
-			setWidth(1300);
-		} else {
-			setHeight(535);
-			setWidth(1280);
-		}
-	}, [isSmallScreen, isMediumScreen]);
+  useEffect(() => {
+    if (isSmallScreen) {
+      setHeight(800);
+      setWidth(360);
+    } else if (isMediumScreen) {
+      setHeight(1800);
+      setWidth(1300);
+    } else {
+      setHeight(695);
+      setWidth(1535);
+    }
+  }, [isSmallScreen, isMediumScreen]);
 
-	useEffect(() => {
-		if ("geolocation" in navigator) {
-			navigator.geolocation.getCurrentPosition(function (position) {
-				const { latitude, longitude } = position.coords;
-				setLatitude(latitude);
-				setLongitude(longitude);
-				setShowMap(true);
-			});
-		} else {
-			alert("Geolocation is not available in this browser.");
-			setShowMap(true);
-		}
-	}, []);
-	const defaultLatitude = 22.521196498344402;
-	const defaultLongitude = 88.46075746320318;
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+        setShowMap(true);
+      });
+    } else {
+      alert("Geolocation is not available in this browser.");
+      setShowMap(true);
+    }
+  }, []);
+  const defaultLatitude = 22.521196498344402;
+  const defaultLongitude = 88.46075746320318;
 
-	const initialViewState = {
-		longitude: Longitude || defaultLongitude,
-		latitude: Latitude || defaultLatitude,
-		zoom: 16,
-	};
-	console.log(Longitude);
-	console.log(Latitude);
+  const initialViewState = {
+    longitude: Longitude || defaultLongitude,
+    latitude: Latitude || defaultLatitude,
+    zoom: 18,
+  };
+  console.log(Longitude);
+  console.log(Latitude);
 
-	return (
-		<div className="w-screen h-screen">
-			{<Navbar className="h-screen" />}
-			{showMap && (
-				<div>
-					<ReactMapGL
-						mapboxAccessToken="pk.eyJ1IjoiYWJpcmZyb21raXRjaGVuIiwiYSI6ImNsanF2OWtpbzAxdWwzZHZzanpkYTByaGoifQ.5h9oEcPU2VSNjvgQmjV-AA"
-						initialViewState={initialViewState}
-						style={{ width: width, height: height }}
-						mapStyle="mapbox://styles/mapbox/outdoors-v12"
-					>
-						{showPopup && (
-							<Popup
-								latitude={Latitude}
-								longitude={Longitude}
-								closeButton={true}
-								closeOnClick={true}
-								onClose={() => togglePopup(false)}
-								anchor="top"
-							>
-								<div>Pop up marker</div>
-							</Popup>
-						)}
-						<Marker
-							latitude={Latitude}
-							longitude={Longitude}
-							offsetLeft={-20}
-							offsetTop={-30}
-						>
-							<img
-								onClick={() => togglePopup(true)}
-								style={{ height: 50, width: 50 }}
-								src={dot}
-								alt="Marker"
-							/>
-						</Marker>
-						<Link to="/payment">
-							<Marker
-								latitude={22.52161900483102}
-								longitude={88.46066619912584}
-								offsetLeft={-20}
-								offsetTop={-30}
-							>
-								<img
-									onClick={() => togglePopup(true)}
-									style={{ height: 50, width: 50 }}
-									src={marker}
-									alt="Marker"
-								/>
-							</Marker>
-						</Link>
+  return (
+    <div className="w-screen h-screen">
+      {<Navbar className="h-screen" />}
+      {showMap && (
+        <div>
+          <ReactMapGL
+            mapboxAccessToken="pk.eyJ1IjoiYWJpcmZyb21raXRjaGVuIiwiYSI6ImNsanF2OWtpbzAxdWwzZHZzanpkYTByaGoifQ.5h9oEcPU2VSNjvgQmjV-AA"
+            initialViewState={initialViewState}
+            style={{ width: width, height: height }}
+            mapStyle="mapbox://styles/mapbox/outdoors-v12"
+          >
+            {showPopup && (
+              <Popup
+                latitude={Latitude}
+                longitude={Longitude}
+                closeButton={true}
+                closeOnClick={true}
+                onClose={() => togglePopup(false)}
+                anchor="top"
+              >
+                <div>Pop up marker</div>
+              </Popup>
+            )}
+            <Marker
+              latitude={Latitude}
+              longitude={Longitude}
+              offsetLeft={-20}
+              offsetTop={-30}
+            >
+              <img
+                onClick={() => togglePopup(true)}
+                style={{ height: 50, width: 50 }}
+                src={dot}
+                alt="Marker"
+              />
+            </Marker>
+            <Link to="/payment">
+              <Marker
+                latitude={22.52161900483102}
+                longitude={88.46066619912584}
+                offsetLeft={-20}
+                offsetTop={-30}
+              >
+                <img
+                  onClick={() => togglePopup(true)}
+                  style={{ height: 50, width: 50 }}
+                  src={marker}
+                  alt="Marker"
+                />
+              </Marker>
+            </Link>
 
-						{list.map((item) => (
-							<Link to="/payment" key={item.id}>
-								<Marker
-									latitude={item.lat}
-									longitude={item.long}
-									offsetLeft={-20}
-									offsetTop={-30}
-								>
-									<img
-										onClick={() => togglePopup(true)}
-										style={{ height: 50, width: 50 }}
-										src={marker}
-										alt="Marker"
-									/>
-								</Marker>
-							</Link>
-						))}
-					</ReactMapGL>
-				</div>
-			)}
-		</div>
-	);
+            {list.map((item) => (
+              <Link to="/payment" key={item.id}>
+                <Marker
+                  latitude={item.lat}
+                  longitude={item.long}
+                  offsetLeft={-20}
+                  offsetTop={-30}
+                >
+                  <img
+                    onClick={() => togglePopup(true)}
+                    style={{ height: 50, width: 50 }}
+                    src={marker}
+                    alt="Marker"
+                  />
+                </Marker>
+              </Link>
+            ))}
+          </ReactMapGL>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -7,108 +7,7 @@ function Navbar() {
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
-  
-	const mobileMenuItems = [
-		{ text: "Home", link: "#" },
-		{ text: "About", link: "#" },
-		{ text: "Services", link: "#" },
-		{ text: "Contact", link: "#" },
-	];
 
-	return (
-		<div>
-			<nav className="bg-yellow-500 p-4">
-				<div className="container mx-auto">
-					<div className=" sm:hidden flex justify-between">
-						<div>
-							<button
-								onClick={toggleMobileMenu}
-								className="text-black hover:text-white focus:outline-none focus:text-white"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-6 w-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									{isMobileMenuOpen ? (
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M6 18L18 6M6 6l12 12"
-										/>
-									) : (
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M4 6h16M4 12h16M4 18h16"
-										/>
-									)}
-								</svg>
-							</button>
-						</div>
-
-						<DropdownMenu />
-					</div>
-
-					<div
-						className={`absolute top-0 right-0 h-full bottom-0 bg-yellow-500 transition-all duration-300 z-20 ${
-							isMobileMenuOpen ? "w-64" : "w-0"
-						} sm:hidden`}
-					>
-						<ul className="text-black py-4 sm:flex-1 sm:justify-end sm:space-x-4 sm:mr-4">
-							{mobileMenuItems.map((item, index) => (
-								<li key={index} className="mb-2 ml-4 py-6 sm:mb-0">
-									<a href={item.link} className="hover:underline">
-										{item.text}
-									</a>
-								</li>
-							))}
-						</ul>
-					</div>
-
-					{/* Desktop Menu (Hidden on Small Screens) */}
-					<div className="hidden sm:flex justify-between">
-						<div className="text-yellow-300 font-bold text-xl flex">
-							<p className="bg-yellow-400 font-sans text-black mx-1 rounded-sm text-xl">
-								P
-							</p>
-							PARK <span className="text-white mx-2"> EASE</span>
-						</div>
-
-						<div>
-							<DropdownMenu />
-						</div>
-						<ul className="text-black text-md flex">
-							<li className="ml-4">
-								<a href="#" className="hover:bg-white rounded-md p-2">
-									Home
-								</a>
-							</li>
-							<li className="ml-4">
-								<a href="#" className="hover:bg-white rounded-md p-2">
-									About
-								</a>
-							</li>
-							<li className="ml-4">
-								<a href="#" className="hover:bg-white rounded-md p-2">
-									Services
-								</a>
-							</li>
-							<li className="ml-4">
-								<a href="#" className="hover:bg-white rounded-md p-2">
-									Contact
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</div>
-	);
   const mobileMenuItems = [
     { text: "Home", link: "#" },
     { text: "About", link: "#" },
@@ -119,42 +18,46 @@ function Navbar() {
   return (
     <div>
       <nav className="bg-yellow-500 p-4">
-        <div className="container mx-auto flex items-center justify-between">
-          <div className="block sm:hidden">
-            <button
-              onClick={toggleMobileMenu}
-              className="text-black hover:text-white focus:outline-none focus:text-white"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+        <div className="container mx-auto">
+          <div className=" sm:hidden flex justify-between">
+            <div>
+              <button
+                onClick={toggleMobileMenu}
+                className="text-black hover:text-white focus:outline-none focus:text-white"
               >
-                {isMobileMenuOpen ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                )}
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {isMobileMenuOpen ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  )}
+                </svg>
+              </button>
+            </div>
+
+            <DropdownMenu />
           </div>
 
           <div
-            className={`absolute top-0 right-0 h-full bg-yellow-500 transition-all duration-300 z-20 ${
+            className={`absolute top-0 right-0 h-full bottom-0 bg-yellow-500 transition-all duration-300 z-20 ${
               isMobileMenuOpen ? "w-64" : "w-0"
-            } sm:hidden lg:hidden`}
+            } sm:hidden`}
           >
             <ul className="text-black py-4 sm:flex-1 sm:justify-end sm:space-x-4 sm:mr-4">
               {mobileMenuItems.map((item, index) => (
@@ -167,35 +70,36 @@ function Navbar() {
             </ul>
           </div>
 
-          {/* (Search Bar)
-          <div className="sm:justify-center items-center flex">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="px-2 py-1 rounded-md bg-gray-100 text-gray-800 focus:outline-none"
-            />
-          </div> */}
-
           {/* Desktop Menu (Hidden on Small Screens) */}
-          <div className="hidden sm:flex sm:justify-end">
-            <ul className="text-black flex justify-end">
+          <div className="hidden sm:flex justify-between">
+            <div className="text-yellow-300 font-bold text-xl flex">
+              <p className="bg-yellow-400 font-sans text-black mx-1 rounded-sm text-xl">
+                P
+              </p>
+              PARK <span className="text-white mx-2"> EASE</span>
+            </div>
+
+            <div>
+              <DropdownMenu />
+            </div>
+            <ul className="text-black text-md flex">
               <li className="ml-4">
-                <a href="#" className="hover:underline">
+                <a href="#" className="hover:bg-white rounded-md p-2">
                   Home
                 </a>
               </li>
               <li className="ml-4">
-                <a href="#" className="hover:underline">
+                <a href="#" className="hover:bg-white rounded-md p-2">
                   About
                 </a>
               </li>
               <li className="ml-4">
-                <a href="#" className="hover:underline">
+                <a href="#" className="hover:bg-white rounded-md p-2">
                   Services
                 </a>
               </li>
               <li className="ml-4">
-                <a href="#" className="hover:underline">
+                <a href="#" className="hover:bg-white rounded-md p-2">
                   Contact
                 </a>
               </li>
@@ -206,5 +110,4 @@ function Navbar() {
     </div>
   );
 }
-
 export default Navbar;
